268ba9eed8e84dd5543d46797c0963564959c581
Matrim Pathak <matrimpathak1999@gmail.com>
2024-10-14 23:47:39 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+	return (
+		<div className="">
+			
+		</div>
+	);
+}
