f029cf6f9a19e6ec0e65d175f55e209346b5033d
Matrim Pathak <matrimpathak1999@gmail.com>
2024-10-15 16:54:15 +0530

    Added Custom Middleware for appwrite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+	const router = useRouter();
+	const { data, isLoading } = useCurrent();
+	const { mutate } = useLogout();
+	useEffect(() => {
+		if (!data && !isLoading) {
+			router.push("/sign-in");
+		}
+	}, [data]);
+
 	return (
 		<div className="">
-			
+			Only visible to authorized users
+			<Button onClick={() => mutate()}>Logout</Button>
 		</div>
 	);
 }
